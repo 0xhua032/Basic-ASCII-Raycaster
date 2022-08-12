fa7566e535dd2614fc29f30589ad96e236d6a611
Aslan <aslambek.hudajev@regionuppsala.se>
2022-08-12 08:29:30 +0200

    added the console game engine and reformated the code for it

--- Basic ASCII Raycaster/Source.cpp
+++ Basic ASCII Raycaster/Source.cpp
@@ -1,99 +1,81 @@
 #include <iostream>
-#include <Windows.h>
-#include <chrono>
+#include <algorithm>
+#include "olcConsoleGameEngine.h"
 
-int main()
+class Game : public olcConsoleGameEngine
 {
-
-	int screenWidth = 120;
-	int screenHeight = 40;
-
-	float playerPosX = 8.0f;
-	float playerPosY = 8.0f;
-	float playerPosZ = 8.0f;
-	float playerFOV = 3.14159 / 4.0;
-
-	int mapWidth = 16;
-	int mapHeight = 16;
-	
-	float depth = 16.0f;
-
-	wchar_t *screen = new wchar_t[screenWidth * screenHeight];
-	HANDLE console = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-
-	SetConsoleActiveScreenBuffer(console);
-	DWORD bytesWritten = 0;
-
-	std::wstring map;
-
-	map += L"################";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#.....####.....#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"#.......#......#";
-	map += L"#.......#......#";
-	map += L"#.......#......#";
-	map += L"#.......########";
-	map += L"#..............#";
-	map += L"#..............#";
-	map += L"################";
-
-	auto tp1 = std::chrono::system_clock::now();
-	auto tp2 = std::chrono::system_clock::now();
-
-
-	while (1)
+public:
+	Game()
 	{
-		//Time
-		//Get the delta time
-		tp2 = std::chrono::system_clock::now();
-		std::chrono::duration<float> tdeltaTime = tp2 - tp1;
-		tp1 = tp2;
+		m_sAppName = L"Textured Raycaster";
+	}
 
-		float deltaTime = tdeltaTime.count();
+	virtual bool OnUserCreate()
+	{
+		//2D map
+		//This is the 2D map. 
+		//# = wall
+		//. = floor
+		map += L"################";
+		map += L"#..............#";
+		map += L"#..............#";
+		map += L"#.....####.....#";
+		map += L"#..............#";
+		map += L"#..............#";
+		map += L"#..............#";
+		map += L"#..............#";
+		map += L"#..............#";
+		map += L"#.......#......#";
+		map += L"#.......#......#";
+		map += L"#.......#......#";
+		map += L"#.......########";
+		map += L"#..............#";
+		map += L"#..............#";
+		map += L"################";
 
+
+		return true;
+	}
+
+	virtual bool OnUserUpdate(float fElapsedTime)
+	{
 		//Controls
-		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)
-			playerPosZ -= 0.6f * deltaTime;
-		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)
-			playerPosZ += 0.6f * deltaTime;
-		if (GetAsyncKeyState((unsigned short)'W') & 0x8000)
+		if (m_keys[L'A'].bHeld)
+			playerPosZ -= turningSpeed * fElapsedTime;
+		if (m_keys[L'D'].bHeld)
+			playerPosZ += turningSpeed * fElapsedTime;
+		if (m_keys[L'W'].bHeld)
 		{
-			playerPosX += sinf(playerPosZ) * 5.0f * deltaTime;
-			playerPosY += cosf(playerPosZ) * 5.0f * deltaTime;
+			playerPosX += sinf(playerPosZ) * speed * fElapsedTime;
+			playerPosY += cosf(playerPosZ) * speed * fElapsedTime;
 
 			//Collision detection
 			//Simple correctional collision detection
 			if (map[(int)playerPosY * mapWidth + (int)playerPosX] == '#')
 			{
-				playerPosX -= sinf(playerPosZ) * 5.0f * deltaTime;
-				playerPosY -= cosf(playerPosZ) * 5.0f * deltaTime;
+				playerPosX -= sinf(playerPosZ) * speed * fElapsedTime;
+				playerPosY -= cosf(playerPosZ) * speed * fElapsedTime;
 			}
 		}
-		if (GetAsyncKeyState((unsigned short)'S') & 0x8000)
+		if (m_keys[L'S'].bHeld)
 		{
-			playerPosX -= sinf(playerPosZ) * 5.0f * deltaTime;
-			playerPosY -= cosf(playerPosZ) * 5.0f * deltaTime;
-			
+			playerPosX -= sinf(playerPosZ) * speed * fElapsedTime;
+			playerPosY -= cosf(playerPosZ) * speed * fElapsedTime;
+
 			//Collision detection
 			//Simple correctional collision detection
 			if (map[(int)playerPosY * mapWidth + (int)playerPosX] == '#')
 			{
-				playerPosX += sinf(playerPosZ) * 5.0f * deltaTime;
-				playerPosY += cosf(playerPosZ) * 5.0f * deltaTime;
+				playerPosX += sinf(playerPosZ) * speed * fElapsedTime;
+				playerPosY += cosf(playerPosZ) * speed * fElapsedTime;
 			}
 		}
 
-
-		for (size_t x = 0; x < screenWidth; x++)
+		//Drawing
+		for (size_t x = 0; x < ScreenWidth(); x++)
 		{
-			float rayAngle = (playerPosZ - playerFOV / 2.0f) + ((float)x / (float)screenWidth) * playerFOV;
-			
+			float rayAngle = (playerPosZ - playerFOV / 2.0f) + ((float)x / (float)ScreenWidth()) * playerFOV;
+
 			float distanceToWall = 0;
 			bool hitWall = false;
 			bool boundary = false;
@@ -111,22 +93,22 @@ int main()
 				testX = (int)(playerPosX + eyeX * distanceToWall);
 				testY = (int)(playerPosY + eyeY * distanceToWall);
 
-				if(testX < 0 || testX >= mapWidth >> testY < 0 || testY >= mapHeight)
+				if (testX < 0 || testX >= mapWidth >> testY < 0 || testY >= mapHeight)
 				{
 					hitWall = true;
 					distanceToWall = depth;
 				}
 				else
 				{
-					if(map[testY * mapWidth + testX] == '#')
+					if (map[testY * mapWidth + testX] == '#')
 					{
 						hitWall = true;
 					}
 				}
 			}
 
-			int ceiling = (float)(screenHeight / 2.0) - screenHeight / (float)distanceToWall;
-			int floor = screenHeight - ceiling;
+			int ceiling = (float)(ScreenHeight() / 2.0) - ScreenHeight() / (float)distanceToWall;
+			int floor = ScreenHeight() - ceiling;
 
 			short shade = ' ';
 			short ceilingShade = ' ';
@@ -139,33 +121,55 @@ int main()
 			else if (distanceToWall < depth)				shade = 0x2591;
 			else											shade = ' ';
 
-			for (size_t y = 0; y < screenHeight; y++)
+			for (size_t y = 0; y < ScreenHeight(); y++)
 			{
 				//Ceiling and floor shading
 				//Determine what shade the ceiling and floor should have, depending on its distance from the player
-				float b = 1.0f - (((float)y - screenHeight / 2.0) / ((float)screenHeight / 2.0f));
-				if		(b < 0.25)							ceilingShade = 'x';
+				float b = 1.0f - (((float)y - ScreenHeight() / 2.0) / ((float)ScreenHeight() / 2.0f));
+				if (b < 0.25)							ceilingShade = 'x';
 				else if (b < 0.50)							ceilingShade = ',';
 				else if (b < 0.75)							ceilingShade = '-';
 				else if (b < 0.90)							ceilingShade = '.';
 				else										ceilingShade = ' ';
 
 				if (y < ceiling)
-					screen[y * screenWidth + x] = ceilingShade;
+					Draw(x, y, L' ');
 				else if (y > ceiling && y <= floor)
-					screen[y * screenWidth + x] = shade;
+					Draw(x, y, shade);
 				else
 				{
-					screen[y * screenWidth + x] = ceilingShade;
+					Draw(x, y, ceilingShade);
 				}
 
 			}
 		}
-
-		screen[screenWidth * screenHeight - 1] = '\0';
-		WriteConsoleOutputCharacter(console, screen, screenWidth * screenHeight, { 0, 0 }, &bytesWritten);
+		return true;
 	}
 
-	return 0;
+private:
+	float playerPosX = 8.0f;
+	float playerPosY = 8.0f;
+	float playerPosZ = 8.0f;
+	float playerFOV = 3.14159 / 4.0;
 
+	int mapWidth = 16;
+	int mapHeight = 16;
+
+	float depth = 16.0f;
+
+	float speed = 5.0f;
+	float turningSpeed = 9.0f;
+
+
+	std::wstring map;
+
+};
+
+int main()
+{
+	Game game;
+	game.ConstructConsole(120, 80, 8, 8);
+	game.Start();
+
+	return 0;
 }
